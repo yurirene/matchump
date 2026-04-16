6fc0a88500ed2fb5b812d05799b82226b30e1c7d
Yuri Ferreira <yurirene@gmail.com>
2026-04-15 22:41:20 -0300

    removendo fake

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PerguntaSeeder::class,
-            MatchUserSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
